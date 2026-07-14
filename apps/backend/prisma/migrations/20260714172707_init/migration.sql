150f0755f7e23dd79ea803bdd94f270f7a57887a
Edudrolhe <edudrolhe@gmail.com>
2026-07-14 14:54:17 -0300

    feat: add new models to Prisma schema

---
+++ apps/backend/prisma/migrations/20260714172707_init/migration.sql
@@ -0,0 +1,78 @@
+-- CreateEnum
+CREATE TYPE "Staus" AS ENUM ('PENDENTE', 'APROVADO', 'REPROVADO', 'CANCELADO');
+
+-- CreateEnum
+CREATE TYPE "FormaPagamento" AS ENUM ('PIX', 'BOLETO', 'CARTAO_CREDITO', 'CARTAO_DEBITO', 'DINHEIRO');
+
+-- CreateTable
+CREATE TABLE "Produto" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "marca" TEXT NOT NULL,
+    "modelo" TEXT NOT NULL,
+    "imagem" TEXT NOT NULL,
+    "nota" DOUBLE PRECISION NOT NULL,
+    "videoReview" TEXT NOT NULL,
+    "tags" TEXT[],
+    "precoBase" DOUBLE PRECISION NOT NULL,
+    "precoPromocional" DOUBLE PRECISION NOT NULL,
+    "menorPreco" DOUBLE PRECISION NOT NULL,
+    "maiorPreco" DOUBLE PRECISION NOT NULL,
+    "precoMedio" DOUBLE PRECISION NOT NULL,
+    "especificacoes" JSONB NOT NULL,
+
+    CONSTRAINT "Produto_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pedido" (
+    "id" SERIAL NOT NULL,
+    "total" DOUBLE PRECISION NOT NULL,
+    "data" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "Staus" NOT NULL DEFAULT 'PENDENTE',
+    "formaPagamento" "FormaPagamento" NOT NULL,
+
+    CONSTRAINT "Pedido_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PedidoEntrega" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT NOT NULL,
+    "endereco" TEXT NOT NULL,
+    "numero" TEXT NOT NULL,
+    "complemento" TEXT,
+    "bairro" TEXT NOT NULL,
+    "cidade" TEXT NOT NULL,
+    "estado" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "pedidoId" INTEGER NOT NULL,
+
+    CONSTRAINT "PedidoEntrega_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ItemPedido" (
+    "id" SERIAL NOT NULL,
+    "quantidade" INTEGER NOT NULL,
+    "precoUnitario" DOUBLE PRECISION NOT NULL,
+    "produtoId" INTEGER NOT NULL,
+    "pedidoId" INTEGER NOT NULL,
+
+    CONSTRAINT "ItemPedido_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PedidoEntrega_pedidoId_key" ON "PedidoEntrega"("pedidoId");
+
+-- AddForeignKey
+ALTER TABLE "PedidoEntrega" ADD CONSTRAINT "PedidoEntrega_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "Pedido"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ItemPedido" ADD CONSTRAINT "ItemPedido_produtoId_fkey" FOREIGN KEY ("produtoId") REFERENCES "Produto"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ItemPedido" ADD CONSTRAINT "ItemPedido_pedidoId_fkey" FOREIGN KEY ("pedidoId") REFERENCES "Pedido"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
